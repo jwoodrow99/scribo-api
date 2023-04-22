1acbd6e9d071350addbc9a3aa1c86fafd84c820d
Jack Woodrow <jackwoodrow99@gmail.com>
2023-04-21 21:36:55 -0400

    Created user table

---
+++ app/Models/User.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  public static table = 'users'
+
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public email: string
+
+  @column()
+  public username: string
+
+  @column()
+  public password: string
+
+  @column()
+  public verified: boolean
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
